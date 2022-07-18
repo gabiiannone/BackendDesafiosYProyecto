3de47f07d63147a6db0f0efe42dfbe4067ef6e10
Gabriel Iannone <giannonearc@gmail.com>
2022-07-18 19:11:50 -0300

    Creacion del repositorio desafios y proyecto final, first commit

---
+++ desafio1.js
@@ -0,0 +1,14 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas) {
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+
+    }
+
+
+
+
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+let myArray = [1,2,3,4,5];
+
+console.log(myArray);
+
+
+
+
+
+
+
+
+
+
+const user = {nombre:"gabriel", edad: 36, seriesFavoritas: ["Picky Blinders","The Sinner","Stranger Things"]};
+console.log(user);
